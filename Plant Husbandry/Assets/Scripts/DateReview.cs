cbd135b7bc15b1e8b8d4a201853c68af191e61d7
AIE\s210449 <s210449@students.aie.edu.au>
2022-07-06 14:14:55 +0930

    Imported indieflower font and berkshire swash font, fixed ui switiching issue, added date review UI

---
+++ Plant Husbandry/Assets/Scripts/DateReview.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DateReview : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //that was a rotten date!
+        //That date was un-beet-able
+        //Where have you bean all my life?
+        //Lettuce be just friends
+        //he makes my heart beet!
+        // peas be mine
+        //he so radishing
+        //I find you very ap-peel-ing
+        //we don't make a great pear
+        //I'm bananas for you, let's never split
+        //thanks a bunch
+        // I don't like you berry much
+
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
