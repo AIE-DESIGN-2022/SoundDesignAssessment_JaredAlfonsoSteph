69e87c43ac907b4d37d1e75739d4b023692c918a
AIE\s210449 <s210449@students.aie.edu.au>
2022-07-06 15:26:21 +0930

    added customer name generator and plant name generator {plant name genreator isn't used yet)

---
+++ Plant Husbandry/Assets/Scripts/CustomerNameGenerator.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CustomerNameGenerator : MonoBehaviour
+{
+
+    public string[] firstNames = new string[50];
+    public string customerFirstName;
+    public int customerFirstNameNumber;
+
+    public string[] lastNames = new string[50];
+    public string customerLastName;
+    public int customerLastNameNumber;
+
+    public TextMeshProUGUI customerNameText;
+
+    public string[] plantNames = new string[50];
+    public string plantName;
+    public int plantNameNumber;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateCustomerName();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateCustomerName()
+    {
+        customerFirstNameNumber = Random.Range(0, firstNames.Length);
+        customerFirstName = firstNames[customerFirstNameNumber];
+
+
+
+        customerLastNameNumber = Random.Range(0, lastNames.Length);
+        customerLastName = lastNames[customerLastNameNumber];
+
+        customerNameText.text = customerFirstName + " " + customerLastName;
+    }
+
+    public void GeneratePlantName()
+    {
+
+    }
+}
